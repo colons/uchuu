1e561055e7a4cb390e59c428e49ca7b71ad0ef1e
colons <code@colons.co>
2019-05-23 09:59:33 +0100

    Fix sharing from the YouTube app.
    Closes #10.

--- Watch in uchuu/ShareViewController.swift
+++ Watch in uchuu/ShareViewController.swift
@@ -6,27 +6,36 @@ class ShareViewController: UIViewController {
         return
     }
     
+    func openUchuu(_ videoUrl: URL) {
+        var targetUrl = URLComponents(string: "uchuu://play")!
+
+        targetUrl.queryItems = [
+            URLQueryItem(name: "url", value: videoUrl.absoluteString),
+        ]
+        var responder: UIResponder? = self as UIResponder
+        let selector = #selector(self.openURL(_:))
+
+        while responder != nil {
+            if responder!.responds(to: selector) && responder != self {
+                responder!.perform(selector, with: targetUrl.url!)
+                break
+            }
+            responder = responder?.next
+        }
+        exit(0)
+    }
+
     override func viewDidAppear(_ animated: Bool) {
         let item = self.extensionContext!.inputItems.first as! NSExtensionItem
-        let attachment = item.attachments!.first
-        attachment!.loadItem(forTypeIdentifier: "public.url", options: nil, completionHandler: { urlItem, error in
-            let videoUrl = urlItem as! URL
-            var targetUrl = URLComponents(string: "uchuu://play")!
-
-            targetUrl.queryItems = [
-                URLQueryItem(name: "url", value: videoUrl.absoluteString),
-            ]
-            var responder: UIResponder? = self as UIResponder
-            let selector = #selector(self.openURL(_:))
-            
-            while responder != nil {
-                if responder!.responds(to: selector) && responder != self {
-                    responder!.perform(selector, with: targetUrl.url!)
-                    break
-                }
-                responder = responder?.next
-            }
-            exit(0)
-        })
+        for attachment in item.attachments! {
+            attachment.loadItem(forTypeIdentifier: "public.url", options: nil, completionHandler: { urlItem, error in
+                guard let videoUrl = urlItem as! URL? else { return }
+                self.openUchuu(videoUrl)
+            })
+            attachment.loadItem(forTypeIdentifier: "public.plain-text", options: nil, completionHandler: { urlItem, error in
+                guard let videoUrl = URL(string: urlItem as! String) else { return }
+                self.openUchuu(videoUrl)
+            })
+        }
     }
 }
